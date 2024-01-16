d827d1915025b1fb381735c9009c50abe9a00aef
Devendra Kumar Dubey <146335880+Devcodes24@users.noreply.github.com>
2024-01-17 03:08:44 +0530

    Create Day15-PlaywithClasses.cpp
    Playing with classes😉 .

---
+++ Day15-PlaywithClasses.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+//creating class
+class harsh{
+
+    public:
+    int h=32;
+    int hp=40;
+    
+    //constructor
+    harsh(int o){
+        int k=o;
+        cout<<k<<" Done👍";
+        
+    }
+};
+
+
+int main()
+{
+    
+    harsh p(8);
+    return 0;
+    
+}
